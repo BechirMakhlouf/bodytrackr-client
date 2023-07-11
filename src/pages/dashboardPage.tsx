5c8be0f08cfa8960f87044307cb353e3eae2663c
bechirMakhlouf <bechirM@proton.me>
2023-07-11 11:18:32 +0100

    add sort for weightlog

--- src/pages/dashboardPage.tsx
+++ src/pages/dashboardPage.tsx
@@ -15,6 +15,13 @@ function getWeightLogFromLocaleStorage(): Weight[] {
   return weightLog;
 }
 
+function sortWeightLog(weightLog: Weight[]): Weight[] {
+  return weightLog.sort((a, b) => {
+    a.date.setHours(0, 0, 0, 0);
+    b.date.setHours(0, 0, 0, 0);
+    return a.date > b.date ? -1 : 1;
+  });
+}
 export default function DashboardPage() {
   const [weightLog, setWeightLog] = useState(getWeightLogFromLocaleStorage());
   const weightInput = useRef({} as HTMLInputElement);
@@ -32,12 +39,15 @@ export default function DashboardPage() {
           const submittedWeight: Weight = {
             weightKg: Number(weightInput.current.value),
             date: new Date(dateInput.current.value),
-          }
-
-          setWeightLog(() => [...weightLog, submittedWeight])
+          };
+          const updatedWeightLog = sortWeightLog([
+            ...weightLog,
+            submittedWeight,
+          ]);
 
           localStorage.setItem("weightLog", JSON.stringify(weightLog));
-          console.log(weightLog);
+
+          setWeightLog(() => updatedWeightLog);
         }}
       >
         <input ref={weightInput} type="number" name="weight" />
